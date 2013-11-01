17822e8597f30b5568df880abba1166165ca375e
Deepak Verma <deepak.23.verma@gmail.com>
2013-11-01 04:41:50 -0400

    most of referral index page complete

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -2,6 +2,10 @@ class ReferralsController < ApplicationController
   # before_filter :require_login
   before_filter :set_user
 
+  def index
+    @referrals = @user.referrals
+  end
+
   def new
     @referral = @user.referrals.build
   end
