e817c2b4ef9077ba73a0a3b08f7544643920f266
Deepak Verma <deepak.23.verma@gmail.com>
2013-10-31 21:33:27 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EprofReferral::Application.config.secret_token = '7356d67b8739e99545556d7b87e3865548f5a40ef0c7d8fc853542bce9e5740eb1af58ca254cc926d0dd7443a5c88f3c89ee19ab6d5024c3b07c1d958bda9bd7'
